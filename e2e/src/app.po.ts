fc3fcf3812ce24716b97f8ccf5a53ae9f56e32ee
Dustin Barnett <dustin.barnett@DTG.local>
2019-09-26 09:47:50 -0400

    UI components for login, user, navbar and core

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,10 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+  getHeadingText() {
+    return element(by.css('app-root h3')).getText();
+  }
+  getNavbar() {
+    return element(by.css('app-navbar h1'));
+  }
 }
